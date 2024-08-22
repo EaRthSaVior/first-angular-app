59c50c9354ce70d61c123eedcbe0f2817e587b40
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-08-23 01:08:49 +0800

    add user and task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-
+import { DUMMY_USERS } from './user/dummy-users';
+import { CommonModule } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    CommonModule,
+    TasksComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  users = DUMMY_USERS;
+  selectedUser = 'u1';
+  get selectedUserObj() {
+    return this.users.find((user) => user.id === this.selectedUser)!;
+  }
+  onSelectUser(id: string) {
+    this.selectedUser = id;
+  }
 }
