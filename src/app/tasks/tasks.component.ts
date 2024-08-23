b72d0849173bebf8bf89307951ef5d9ef94f2d82
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-08-23 19:33:29 +0800

    learn angular model and form

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from './task/dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { NgIf } from '@angular/common';
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent, NgIf],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) userId!: String;
+  @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false;
   tasks = dummyTasks;
 
   get selectedUserTasks() {
@@ -19,5 +23,21 @@ export class TasksComponent {
   }
   onTaskCompleted(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  addNewTask() {
+    this.isAddingTask = true;
+  }
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+    });
+    this.isAddingTask = false;
   }
 }
